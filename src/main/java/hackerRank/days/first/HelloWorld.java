19e207b227a0e2de9aab04915c34c2caa06d492b
Emin Developer <emin.f.mammadov@gmail.com>
2022-02-14 15:24:30 -0800

    Add Project

--- src/main/java/hackerRank/days/first/HelloWorld.java
+++ src/main/java/hackerRank/days/first/HelloWorld.java
@@ -1,5 +1,17 @@
 package hackerRank.days.first;
 
-public class HelloWorld {
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class HelloWorld {
+    public static void main(String[] args) {
+        String input_string;
+        Scanner sc = new Scanner(System.in);
+        input_string = sc.nextLine();
+        System.out.println("Hello, World.\n" + input_string);
+    }
 }
+
